7e43da58e27a264120aa32949f0971992187f392
Nick Seel <nick.seel@gmail.com>
2019-01-19 16:42:03 -0600

    increase ability damage skill
    example skill increases commando's x by 50%, 100%, and 150% damage (of
    what it would be, not of the player's damage stat)

--- commando_skills.lua
+++ commando_skills.lua
@@ -5,25 +5,25 @@ function CommandoSkillTree()
   local skill_test = Skill:new("fun skill",
       "increase fun by |0|, and decrease enemy fun by |1|.",
       {{"10%%", "20%%", "30%%"}, {"15%%", "35%%", "90%%"}}, 3,
-      "skill", TestSkillEffect.new(), {{0},{1},{2},{3}})
+      nil, TestSkillEffect.new(), {{0},{1},{2},{3}})
   
   local skill_health = Skill:new("bonus health",
       "increases max health by |0|.",
       {{"50", "100", "150", "200", "250"}}, 5,
       "health", FlatHealthSkillEffect.new(), {{0},{50},{100},{150},{200},{250}})
   
-  local skill_3 = Skill:new("test3", "test desc 3 |0|, and more info here |1|.", {{"10%%", "20%%", "30%%"}, {"15%%", "35%%", "90%%"}}, 3, nil)
+  local skill_damage_x = Skill:new("x damage", "increase x damage by |0|%%.", {{"50%%", "100%%", "150%%"}}, 3, "skill", ProjectileDamageSkillEffect:new("commando", 2), {{0},{.5},{1},{1.5}})
   local skill_4 = Skill:new("test4", "test desc 4 |0|, and more info here |1|.", {{"10%%", "20%%", "30%%"}, {"15%%", "35%%", "90%%"}}, 3, nil)
   local skill_5 = Skill:new("test5", "test desc 5 |0|, and more info here |1|.", {{"10%%", "20%%", "30%%"}, {"15%%", "35%%", "90%%"}}, 3, nil)
   local skill_6 = Skill:new("test6", "test desc 6 |0|, and more info here |1|.", {{"10%%", "20%%", "30%%"}, {"15%%", "35%%", "90%%"}}, 3, nil)
   
-  skill_test:addChildren(skill_3, skill_4)
+  skill_test:addChildren(skill_4)
   skill_health:addChildren(skill_4, skill_5)
   skill_5:addChildren(skill_6)
   
   skilltree:addSkill(skill_test, 0.5, 0)
   skilltree:addSkill(skill_health, 1.5, 0)
-  skilltree:addSkill(skill_3, 0, 1)
+  skilltree:addSkill(skill_damage_x, 0, 1)
   skilltree:addSkill(skill_4, 1, 1)
   skilltree:addSkill(skill_5, 2, 1)
   skilltree:addSkill(skill_6, 1.5, 2)
--- skill_effect.lua
+++ skill_effect.lua
@@ -21,18 +21,20 @@ function TestSkillEffect:new(subclass)
   local t = setmetatable({}, { __index = TestSkillEffect })
   
   t.values = {0}
-  t.active = false
   
   if(not subclass) then t:initEffect() end
   return t
 end
-
 function TestSkillEffect:initEffect()
   registercallback("onStep", function()
     if(self.active) then
       Cyclone.terminal.write("test skill: "..tostring(self.values[1]))
     end
   end)
+end
+function TestSkillEffect:setValues(values)
+  self.values = values
+  self.active = (self.values[1] > 0)
 end
 
 
@@ -62,4 +64,66 @@ function FlatHealthSkillEffect:setValues(values)
     player:set("maxhp", _new_maxhp)
     player:set("hp", _new_hp)
   end
+end
+
+
+--change damage of one ability by a percentage (value of 0.5 = 50% damage increase)
+ProjectileDamageSkillEffect = SkillEffect:new(true)
+function ProjectileDamageSkillEffect:new(survivor_name, skill_index, subclass)
+  local t = setmetatable({}, { __index = ProjectileDamageSkillEffect })
+  
+  t.values = {0}
+  t.active = true
+  t.deactivate_at_level_zero = true
+  t.survivor = Survivor.find(survivor_name)
+  t.skill_index = skill_index or 0
+  t.last_used_skill_index = 0
+  t.last_used_skill_timer = 0
+  
+  if(not subclass) then t:initEffect() end
+  return t
+end
+function ProjectileDamageSkillEffect:initEffect()
+  registercallback("onStep", function()
+    --if(self.active) then
+      self.last_used_skill_timer = self.last_used_skill_timer + 1
+    --end
+  end)
+  self.survivor:addCallback("useSkill", function(player, skill)
+    if(self.active) then
+      if(skill == self.skill_index) then
+        self.last_used_skill_timer = 0
+      end
+      self.last_used_skill_index = skill
+    end
+  end)
+  self.survivor:addCallback("onSkill", function(player, skill)
+    if(self.active) then
+      if(skill == self.skill_index) then
+        self.last_used_skill_timer = -1 --set to -1 because onStop increases by 1 immediately afterwards
+      end
+      self.last_used_skill_index = skill
+    end
+  end)
+  registercallback("onFire", function(damager)
+    if(self.active) then
+      for _, player in ipairs(misc.players) do
+        if(player:get("id") == damager:get("parent")) then
+          if(self.skill_index == self.last_used_skill_index and self.last_used_skill_timer == 0) then
+            local _prev_damage = damager:get("damage")
+            local _prev_damage_fake = damager:get("damage_fake")
+            local _new_damage = _prev_damage * (1 + self.values[1])
+            local _new_damage_fake = _prev_damage_fake * (1 + self.values[1])
+            damager:set("damage", _new_damage)
+            damager:set("damage_fake", _new_damage_fake)
+            --Cyclone.terminal.write("set damage "..self.last_used_skill_index.."   timer: "..self.last_used_skill_timer)
+          end
+        end
+      end
+    end
+  end)
+end
+function ProjectileDamageSkillEffect:setValues(values)
+  self.values = values
+  self.active = true--(self.values[1] > 0)
 end
